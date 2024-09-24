fa3861fb7c28bb96c40f2957afe8cbecab39e2f0
xuergo <edison.x.xu@gmail.com>
2024-09-24 21:49:33 +0800

    feat: ✨跑通

--- android/src/main/java/expo/modules/multimoduledemo/ExpoMultiModuleDemoView.kt
+++ android/src/main/java/expo/modules/multimoduledemo/ExpoMultiModuleDemoView.kt
@@ -6,6 +6,7 @@ import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.views.ExpoView
 import android.util.Log
 import android.widget.TextView
+import com.example.mylibrary.TestFile
 
 
 class ExpoMultiModuleDemoView(context: Context, appContext: AppContext) : ExpoView(context, appContext) {
@@ -13,7 +14,7 @@ class ExpoMultiModuleDemoView(context: Context, appContext: AppContext) : ExpoVi
 
     fun setChartData() {
         val textView = TextView(context).apply {
-            text = "Hello World"
+            text = TestFile()
             setTextColor(Color.BLACK)
         }
         addView(textView)
---
+++ example/android/mylibrary/src/main/java/com/example/mylibrary/TestFile.kt
@@ -0,0 +1,8 @@
+package com.example.mylibrary
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+fun TestFile(): String { // 修改返回类型
+    return "Hello from the other module"
+}
